f7566b3755929008534e2c5f414f04a7bbca9819
Skwizz <72879590+Skwizzz@users.noreply.github.com>
2026-06-08 14:49:19 +0200

    V1.01
    Added more command

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import os
 import yt_dlp
 import asyncio
 from discord import FFmpegPCMAudio
-
+import time
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
@@ -33,12 +33,14 @@ def get_queue(guild_id):
 async def on_ready():
 
     print(f"Bot successfully logged in as {bot.user.name}")
+    print(f"Bot ping : {round(bot.latency * 1000)} ms ")
 # COMMANDS FOR THE MUSIC PLAYER
 @bot.command()
 async def join(ctx):
     if ctx.author.voice:
         channel = ctx.author.voice.channel
         await channel.connect()
+
 @bot.command()
 async def leave(ctx):
     if ctx.voice_client:
@@ -101,11 +103,14 @@ async def play(ctx, *, query: str):
 
     if not ctx.voice_client.is_playing():
         play_next(ctx)
+
 @bot.command()
 async def skip(ctx):
+
     if ctx.voice_client and ctx.voice_client.is_playing():
         ctx.voice_client.stop()
         await ctx.send("Skipping ⏭️")
+
 @bot.command()
 async def stop(ctx):
     if ctx.voice_client:
@@ -113,6 +118,123 @@ async def stop(ctx):
         get_queue(ctx.guild.id).clear()
         await ctx.send("Successfully stopped 🛑")
 
+@bot.command()
+async def queue(ctx):
+    q = get_queue(ctx.guild.id)
+
+    embed = discord.Embed(
+        title="🎧 Music Queue",
+        color=discord.Color.purple()
+    )
+
+    if len(q) == 0:
+        embed.description = "Empty ❌"
+        await ctx.send(embed=embed)
+        return
+
+    description = ""
+
+    for i, song in enumerate(q[:10], start=1):  
+        description += f"**{i}.** {song['title']}\n"
+
+    embed.description = description
+
+
+    embed.set_footer(text=f"{len(q)} song(s) in queue")
+
+    await ctx.send(embed=embed)
+@bot.command()
+async def clear(ctx, amount: str):
+    if amount.lower() == "all":
+        deleted = await ctx.channel.purge(limit=None)
+        nb = len(deleted)
+
+        embed = discord.Embed(
+            title="Cleared",
+            description=f"Successfully cleared **{nb} messages** (ALL)",
+            color=discord.Color.purple()
+        )
+
+        msg = await ctx.send(embed=embed)
+        await asyncio.sleep(3)
+        await msg.delete()
+
+    else:
+        amount = int(amount)
+
+        deleted = await ctx.channel.purge(limit=amount + 1)
+        nb = len(deleted) - 1  # enlève la commande
+
+        embed = discord.Embed(
+            title="Cleared",
+            description=f"Successfully cleared **{nb} messages**",
+            color=discord.Color.purple()
+        )
+
+        msg = await ctx.send(embed=embed)
+        await asyncio.sleep(3)
+        await msg.delete()
+@bot.command()
+async def userinfo(ctx, member: discord.Member = None):
+    member = member or ctx.author
+
+    embed = discord.Embed(title="User Info")
+    embed.add_field(name="Name", value=member.name)
+    embed.add_field(name="ID", value=member.id)
+    embed.set_thumbnail(url=member.avatar.url)
+    color=discord.Color.purple()
+    await ctx.send(embed=embed)
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f"My ping is {round(bot.latency * 1000)} ms")
+@bot.command()
+async def roast(ctx,member : discord.Member):
+    await ctx.send(f"{member.mention} restarted ? 🐒 ")
+    await ctx.send("https://tenor.com/view/shut-up-stfu-shut-your-mouth-slap-slapping-gif-8050553153066707611")
+@bot.command()
+async def remind(ctx,time :int,*,message):
+    await ctx.send(f"I'll remind you in {time} seconds ⏳ ")
+    await asyncio.sleep(time)
+    await ctx.send(f"🔔Reminder : {message} {ctx.author.mention} ")
+@bot.command()
+async def stats(ctx):
+    guild = ctx.guild
+    statEmbed = discord.Embed(
+        title="Stats",
+        description=f"Stats of {guild.name}",
+        
+        
+        color=discord.Color.purple()
+        
+    )
+    statEmbed.add_field(name="Members",value=f"👥 Members: {guild.member_count}")
+    statEmbed.add_field(name="Channels",value=f"💬 Channels: {len(guild.channels)}")
+    statEmbed.set_thumbnail(url=guild.icon.url)
+    await ctx.send(embed=statEmbed)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ALL MUSICS COMMAND AS HELP
 @bot.command()
 async def music(ctx):
     musicEmbed = discord.Embed(
@@ -154,30 +276,68 @@ async def music(ctx):
     
     await ctx.send(embed=musicEmbed)
 
-
+# LISTING ALL COMMANDS IN AN EMBED 
 @bot.command()
-async def queue(ctx):
-    q = get_queue(ctx.guild.id)
-
-    embed = discord.Embed(
-        title="🎧 Music Queue",
+async def helpMe(ctx):
+    helpEmbed = discord.Embed(
+        title="Help",
+        description="Listing all Oreo's commands",
+        
         color=discord.Color.purple()
-    )
+        )
+    helpEmbed.set_footer(text="All commands starts with lowercase letters ")
+    helpEmbed.add_field(
+        name="Prefix",
+        value="Use ? before any command to call the bot ❓",
+        inline= True
+        )
+    helpEmbed.add_field(
+        name="Music",
+        value="Displays an embed with all the commands used to play music in a voice channel",
+        inline= True
+        )
+    helpEmbed.add_field(
+        name="Clear",
+        value="Clears a specified number of messages (syntax : ?clear numberOfMessages or ?clear all)",
+        inline= True
+        )    
+    helpEmbed.add_field(
+        name="Userinfo",
+        value="Gets user info (name,ID,profile picture), (syntax ?userinfo @user)",
+        inline= True
+        )   
+    helpEmbed.add_field(
+        name="Ping",
+        value="Checks the bot latency",
+        inline= True
+        ) 
+    helpEmbed.add_field(
+        name="Roast",
+        value="Roasts an user (syntax ?roast @user)",
+        inline= True
+        )    
+    helpEmbed.add_field(
+        name="Remind",
+        value="Reminds you (syntax : ?remind time(in seconds) message)",
+        inline= True
+        )   
+    helpEmbed.add_field(
+        name="Stats",
+        value="Shows the stats of the server",
+        inline= True
+        ) 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    await ctx.send(embed=helpEmbed)
+    
 
-    if len(q) == 0:
-        embed.description = "Empty ❌"
-        await ctx.send(embed=embed)
-        return
-
-    description = ""
-
-    for i, song in enumerate(q[:10], start=1):  
-        description += f"**{i}.** {song['title']}\n"
-
-    embed.description = description
-
-
-    embed.set_footer(text=f"{len(q)} song(s) in queue")
-
-    await ctx.send(embed=embed)
+# RUNS THE BOT WHEN STARTING SCRIPT
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
